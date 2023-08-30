9850d896a51d0de21ba1c7d9ae7262e43ae566ff
Manik Hosen <rahathossenmanik@gmail.com>
2023-08-30 23:28:39 +0600

    horse removed

--- src/pages/Horses.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Horses = () => {
-  return <div>Horses</div>;
-};
-
-export default Horses;
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,7 +2,6 @@ import AllPets from '../pages/AllPets';
 import Birds from '../pages/Birds';
 import Cats from '../pages/Cats';
 import Dogs from '../pages/Dogs';
-import Horses from '../pages/Horses';
 import PetForm from '../pages/PetForm';
 import Reptiles from '../pages/Reptiles';
 
@@ -10,7 +9,6 @@ export const routes = [
   { path: '/', element: <AllPets /> },
   { path: '/dogs', element: <Dogs /> },
   { path: '/cats', element: <Cats /> },
-  { path: '/horses', element: <Horses /> },
   { path: '/birds', element: <Birds /> },
   { path: '/reptiles', element: <Reptiles /> },
   { path: '/pets/register', element: <PetForm /> }
