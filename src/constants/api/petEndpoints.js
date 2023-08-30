169b399549ff9aa722da09ea1fd08ce9a0d75b02
Manik Hosen <rahathossenmanik@gmail.com>
2023-08-30 23:26:28 +0600

    secondary endpoints added

--- src/constants/api/petEndpoints.js
+++ src/constants/api/petEndpoints.js
@@ -3,5 +3,10 @@ export const PET_API = {
   get_by_id: (id) => `/pets/getbyid/${id}`,
   create: () => `/pets/create`,
   edit: (id) => `/pets/update/${id}`,
-  delete: (id) => `/pets/delete/${id}`
+  delete: (id) => `/pets/delete/${id}`,
+  get_all_dog: () => `/pets/getalldog`,
+  get_all_cat: () => `/pets/getallcat`,
+  get_all_bird: () => `/pets/getallbird`,
+  get_all_reptile: () => `/pets/getallreptile`,
+  love: (id) => `/pets/love/${id}`,
 };
