f00e01d51aa7c25fa2db9b575edcfc16890c2a1f
Manik Hosen <rahathossenmanik@gmail.com>
2023-08-31 00:45:50 +0600

    margin bug fixed

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -2,19 +2,23 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { routes } from '../routes/routes';
 import Navbar from './Navbar';
+import Footer from './Footer';
+import { useStyles } from '../constants/common/commonStyles';
 
 const Layout = (props) => {
   const { setThemeMode } = props;
+  const { classes } = useStyles();
   return (
     <>
       <Navbar setThemeMode={setThemeMode} />
-      <div className="mx-20">
+      <div className={classes.mainBody}>
         <Routes>
           {routes.map((route, i) => (
             <Route exact key={i} path={route.path} element={route.element} />
           ))}
         </Routes>
       </div>
+      <Footer />
     </>
   );
 };
