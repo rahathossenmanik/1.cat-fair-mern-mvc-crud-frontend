d939e3beda8693f34ef7c51ebff57cca3396979a
Manik Hosen <rahathossenmanik@gmail.com>
2023-08-30 23:29:15 +0600

    all features done

--- src/pages/AllPets.jsx
+++ src/pages/AllPets.jsx
@@ -1,109 +1,26 @@
 import React, { useState, useEffect } from 'react';
-import { Grid, Container, Card, Image, ActionIcon, Group, Text, Button, Badge, createStyles, rem } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
 import { PET_API } from '../constants/api/petEndpoints';
 import { get } from '../helpers/api_helpers';
-import { Icon } from '@iconify/react';
-
-const useStyles = createStyles((theme) => ({
-  card: {
-    backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.white
-  },
-
-  rating: {
-    position: 'absolute',
-    top: theme.spacing.xs,
-    right: rem(12),
-    pointerEvents: 'none'
-  },
-
-  title: {
-    fontFamily: `Greycliff CF, ${theme.fontFamily}`
-  },
-
-  footer: {
-    padding: `${theme.spacing.xs} ${theme.spacing.lg}`,
-    marginTop: theme.spacing.md,
-    borderTop: `${rem(1)} solid ${theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[2]}`
-  }
-}));
+import Archive from './Archive';
 
 const AllPets = () => {
-  const { classes, theme } = useStyles();
-
+  const [loading, setLoading] = useState(true);
   const [pets, setPets] = useState([]);
+  const [trigger, { toggle }] = useDisclosure(false);
 
   useEffect(() => {
     const getAllPets = async () => {
+      pets?.length === 0 && setLoading(true);
       const response = await get(PET_API.get_all());
       setPets(response);
+      setLoading(false);
     };
     getAllPets();
-  }, []);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [trigger]);
 
-  const shortenAbout = (text) => {
-    if (text.length > 100) {
-      return text.substring(0, 100) + '...';
-    } else {
-      return text;
-    }
-  };
-
-  return (
-    <Container my="md">
-      <Grid>
-        {pets?.map((pet, i) => (
-          <Grid.Col key={i} xs={12} sm={6} md={4}>
-            <Card withBorder padding="lg" radius="md" className={classes.card}>
-              <Card.Section mb="sm">
-                <Image src={pet?.image} alt={'Pet'} height={180} />
-              </Card.Section>
-
-              <Badge className={classes.rating} variant="gradient" gradient={{ from: 'blue', to: 'cyan' }}>
-                {pet?.age} Years
-              </Badge>
-
-              <Badge>{pet?.petType?.label}</Badge>
-
-              <Text fw={700} className={classes.title} mt="xs">
-                {pet?.name}
-              </Text>
-              <Text fz="sm" color="dimmed" lineClamp={4}>
-                {shortenAbout(pet?.about)}
-              </Text>
-
-              <Group mt="lg">
-                {/* <Avatar src={author.image} radius="sm" />
-              <div>
-                <Text fw={500}>{author.name}</Text>
-                <Text fz="xs" c="dimmed">
-                  {author.description}
-                </Text>
-              </div> */}
-              </Group>
-
-              <Card.Section className={classes.footer}>
-                <Group position="apart">
-                  <Group mt="xs">
-                    <Button radius="md" style={{ flex: 1 }}>
-                      View {pet?.name}
-                    </Button>
-                  </Group>
-                  <Group spacing={0} mt="xs">
-                    <ActionIcon>
-                      <Icon icon="mdi:heart-outline" size="1.2rem" color={theme.colors.red[6]} stroke={1.5} />
-                    </ActionIcon>
-                    <ActionIcon>
-                      <Icon icon="solar:share-outline" size="1.2rem" color={theme.colors.blue[6]} stroke={1.5} />
-                    </ActionIcon>
-                  </Group>
-                </Group>
-              </Card.Section>
-            </Card>
-          </Grid.Col>
-        ))}
-      </Grid>
-    </Container>
-  );
+  return <Archive pets={pets} loading={loading} trigger={toggle} />;
 };
 
 export default AllPets;
---
+++ src/pages/Archive.jsx
@@ -0,0 +1,123 @@
+import React from 'react';
+import {
+  Grid,
+  Container,
+  Card,
+  Image,
+  ActionIcon,
+  Group,
+  Text,
+  Button,
+  Badge,
+  LoadingOverlay,
+} from '@mantine/core';
+import { Icon } from '@iconify/react';
+import { post } from '../helpers/api_helpers';
+import { PET_API } from '../constants/api/petEndpoints';
+import { useNavigate } from 'react-router-dom';
+import { useStyles } from '../constants/common/commonStyles';
+
+const Archive = (props) => {
+  const { pets, loading, trigger } = props;
+  const { classes, theme } = useStyles();
+  const navigate = useNavigate();
+
+  const url =
+    window.location.protocol + '//' + window.location.host + '/pets/single/';
+
+  const shortenAbout = (text) => {
+    if (text.length > 100) {
+      return text.substring(0, 100) + '...';
+    } else {
+      return text;
+    }
+  };
+
+  const onSingleView = (pet) => {
+    navigate(`/pets/single/${pet?._id}`);
+  };
+
+  const onLoveReact = async (id) => {
+    const res = await post(PET_API.love(id));
+    if (res) trigger();
+  };
+
+  const onShareFacebook = (id) => {
+    window.open(
+      `https://www.facebook.com/sharer/sharer.php?u=${url + id}&t=TITLE`,
+      '',
+      'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=300,width=600'
+    );
+    return false;
+  };
+
+  return (
+    <Container
+      my='md'
+      pos='relative'
+      style={{ minHeight: '80vh', minWidth: '100%' }}>
+      <LoadingOverlay visible={loading} overlayBlur={2} />
+      <Grid>
+        {pets?.map((pet, i) => (
+          <Grid.Col key={i} xs={12} sm={6} md={4}>
+            <Card withBorder padding='lg' radius='md' className={classes.card}>
+              <Card.Section mb='sm'>
+                <Image src={pet?.image} alt={'Pet'} height={180} />
+              </Card.Section>
+
+              <Badge
+                className={classes.rating}
+                variant='gradient'
+                gradient={{ from: 'blue', to: 'cyan' }}>
+                {pet?.age} Years
+              </Badge>
+
+              <Badge>{pet?.petType?.label}</Badge>
+
+              <Text fw={700} className={classes.title} mt='xs'>
+                {pet?.name}
+              </Text>
+              <Text fz='sm' color='dimmed' lineClamp={4}>
+                {shortenAbout(pet?.about)}
+              </Text>
+
+              <Card.Section className={classes.footer}>
+                <Group position='apart'>
+                  <Group mt='xs'>
+                    <Button
+                      radius='md'
+                      style={{ flex: 1 }}
+                      onClick={() => onSingleView(pet)}>
+                      View {pet?.name}
+                    </Button>
+                  </Group>
+                  <Group spacing={0} mt='xs' fz='xs'>
+                    {pet?.loveCount}
+                    <ActionIcon onClick={() => onLoveReact(pet?._id)}>
+                      <Icon
+                        icon='mdi:heart-outline'
+                        size='1.2rem'
+                        color={theme.colors.red[6]}
+                        stroke={1.5}
+                      />
+                    </ActionIcon>
+                    <ActionIcon onClick={() => onShareFacebook(pet?._id)}>
+                      <Icon
+                        icon='solar:share-outline'
+                        size='1.2rem'
+                        color={theme.colors.blue[6]}
+                        stroke={1.5}
+                      />
+                    </ActionIcon>
+                  </Group>
+                </Group>
+              </Card.Section>
+            </Card>
+          </Grid.Col>
+        ))}
+      </Grid>
+    </Container>
+  );
+};
+
+export default Archive;
